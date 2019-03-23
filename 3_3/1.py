995d0494d63cbf8dfb97b9e2d891f8fa48df9a0c
yuangezhizao <root@yuangezhizao.cn>
2019-03-23 11:33:59 +0800

    3_3

---
+++ 3_3/1.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao
+    :Time: 2019/3/23 0023 11:20
+    :Site: https://www.yuangezhizao.cn
+    :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
+"""
+# 导入模块，生成模拟数据集
+import tensorflow as tf
+import numpy as np
+
+BATCH_SIZE = 8
+SEED = 23455
+
+# 基于seed产生随机数
+rdm = np.random.RandomState(SEED)
+# 随机数返回32行2列的矩阵 表示32组 体积和重量 作为输入数据集
+X = rdm.rand(32, 2)
+# 从X这个32行2列的矩阵中 取出一行 判断如果和小于1 给Y赋值1 如果和不小于1 给Y赋值0
+# 作为输入数据集的标签（正确答案）
+Y_ = [[int(x0 + x1 < 1)] for (x0, x1) in X]
+print(X)
+print(Y_)
+
+# 1定义神经网络的输入、参数和输出,定义前向传播过程。
+x = tf.placeholder(tf.float32, shape=(None, 2))
+y_ = tf.placeholder(tf.float32, shape=(None, 1))
+
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
+
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+# 2定义损失函数及反向传播方法。
+loss_mse = tf.reduce_mean(tf.square(y - y_))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
+# train_step = tf.train.MomentumOptimizer(0.001,0.9).minimize(loss_mse)
+# train_step = tf.train.AdamOptimizer(0.001).minimize(loss_mse)
+
+# 3生成会话，训练STEPS轮
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    # 输出目前（未经训练）的参数取值。
+    print(sess.run(w1))
+    print(sess.run(w2))
+
+    # 训练模型。
+    STEPS = 3000
+    for i in range(STEPS):
+        start = (i * BATCH_SIZE) % 32
+        end = start + BATCH_SIZE
+        sess.run(train_step, feed_dict={x: X[start:end], y_: Y_[start:end]})
+        if i % 500 == 0:
+            total_loss = sess.run(loss_mse, feed_dict={x: X, y_: Y_})
+            print("After %d training step(s), loss_mse on all data is %g" % (i, total_loss))
+
+    # 输出训练后的参数取值。
+    print(sess.run(w1))
+    print(sess.run(w2))
+
+'''
+C:\Python37\python.exe D:/yuangezhizao/Documents/PycharmProjects/Tensorflow-Study/3_3/1.py
+[[0.83494319 0.11482951]
+ [0.66899751 0.46594987]
+ [0.60181666 0.58838408]
+ [0.31836656 0.20502072]
+ [0.87043944 0.02679395]
+ [0.41539811 0.43938369]
+ [0.68635684 0.24833404]
+ [0.97315228 0.68541849]
+ [0.03081617 0.89479913]
+ [0.24665715 0.28584862]
+ [0.31375667 0.47718349]
+ [0.56689254 0.77079148]
+ [0.7321604  0.35828963]
+ [0.15724842 0.94294584]
+ [0.34933722 0.84634483]
+ [0.50304053 0.81299619]
+ [0.23869886 0.9895604 ]
+ [0.4636501  0.32531094]
+ [0.36510487 0.97365522]
+ [0.73350238 0.83833013]
+ [0.61810158 0.12580353]
+ [0.59274817 0.18779828]
+ [0.87150299 0.34679501]
+ [0.25883219 0.50002932]
+ [0.75690948 0.83429824]
+ [0.29316649 0.05646578]
+ [0.10409134 0.88235166]
+ [0.06727785 0.57784761]
+ [0.38492705 0.48384792]
+ [0.69234428 0.19687348]
+ [0.42783492 0.73416985]
+ [0.09696069 0.04883936]]
+[[1], [0], [0], [1], [1], [1], [1], [0], [1], [1], [1], [0], [0], [0], [0], [0], [0], [1], [0], [0], [1], [1], [0], [1], [0], [1], [1], [1], [1], [1], [0], [1]]
+WARNING:tensorflow:From C:\Python37\lib\site-packages\tensorflow\python\framework\op_def_library.py:263: colocate_with (from tensorflow.python.framework.ops) is deprecated and will be removed in a future version.
+Instructions for updating:
+Colocations handled automatically by placer.
+2019-03-23 11:32:33.948400: I tensorflow/core/platform/cpu_feature_guard.cc:141] Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2
+2019-03-23 11:32:34.686142: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1433] Found device 0 with properties: 
+name: GeForce GTX 965M major: 5 minor: 2 memoryClockRate(GHz): 0.9495
+pciBusID: 0000:01:00.0
+totalMemory: 2.00GiB freeMemory: 1.63GiB
+2019-03-23 11:32:34.691836: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1512] Adding visible gpu devices: 0
+2019-03-23 11:32:35.216343: I tensorflow/core/common_runtime/gpu/gpu_device.cc:984] Device interconnect StreamExecutor with strength 1 edge matrix:
+2019-03-23 11:32:35.219613: I tensorflow/core/common_runtime/gpu/gpu_device.cc:990]      0 
+2019-03-23 11:32:35.221639: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1003] 0:   N 
+2019-03-23 11:32:35.223923: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1115] Created TensorFlow device (/job:localhost/replica:0/task:0/device:GPU:0 with 1372 MB memory) -> physical GPU (device: 0, name: GeForce GTX 965M, pci bus id: 0000:01:00.0, compute capability: 5.2)
+[[-0.8113182   1.4845988   0.06532937]
+ [-2.4427042   0.0992484   0.5912243 ]]
+[[-0.8113182 ]
+ [ 1.4845988 ]
+ [ 0.06532937]]
+2019-03-23 11:32:35.418320: I tensorflow/stream_executor/dso_loader.cc:152] successfully opened CUDA library cublas64_100.dll locally
+After 0 training step(s), loss_mse on all data is 5.13118
+After 500 training step(s), loss_mse on all data is 0.429111
+After 1000 training step(s), loss_mse on all data is 0.409789
+After 1500 training step(s), loss_mse on all data is 0.399923
+After 2000 training step(s), loss_mse on all data is 0.394146
+After 2500 training step(s), loss_mse on all data is 0.390597
+[[-0.7000663   0.91363174  0.0895357 ]
+ [-2.3402493  -0.14641264  0.58823055]]
+[[-0.06024268]
+ [ 0.91956186]
+ [-0.06820709]]
+
+Process finished with exit code 0
+'''
